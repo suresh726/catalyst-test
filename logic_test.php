62c14f6f7a7ff7474da26495414feab2915a69b0
Suresh Lamichhane <suresh.lamichhane726@gmail.com>
2021-07-09 08:19:06 +1000

    comma removed from last iteration

--- logic_test.php
+++ logic_test.php
@@ -5,5 +5,6 @@ $end = 100;
 
 for ($i = $start; $i <= $end; $i++) {
     $result = ($i%3 == 0) ? ($i%5 == 0 ? 'foobar' : 'foo') : ($i%5 == 0 ? 'bar' : $i);
-    echo $result.', ';
+    echo ($i !== $end) ? $result.', ' : $result;
 }
+echo PHP_EOL;
